921bf1e852b47375a4453f488ec8104fe1c14dfb
Kevin Palowa <kevinpalowa@gmail.com>
2022-09-10 11:46:06 +0700

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-5 dark:bg-dark bg-white text-center text-sm mt-10">
+      Copyright Kevin
+    </footer>
+  );
+};
+
+export default Footer;
